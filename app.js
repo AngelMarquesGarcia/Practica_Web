c5ac9d728b83f3316f895c80d5be145d693e94c1
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2022-11-06 20:12:06 +0100

    Started with HTML and created functions to change between pages

---
+++ app.js
@@ -0,0 +1,44 @@
+
+function pagRecetas() {
+    let content = document.getElementById('content');
+    content.innerHTML = 'Recetas' 
+}
+
+
+function pagIngredientes() {
+    let content = document.getElementById('content');
+    content.innerHTML = 'Ingredientes' 
+}
+
+function pagCrearReceta() {
+    let content = document.getElementById('content');
+    content.innerHTML = 'Crear Receta' 
+}
+
+
+function pagCrearIngrediente() {
+    let content = document.getElementById('content');
+    content.innerHTML = 'Crear Ingrediente' 
+}
+
+
+function pagBuscar() {
+    let content = document.getElementById('content');
+    content.innerHTML = 'Buscar' 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
