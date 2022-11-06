f6e1d2773717f4fcf574aa3c06cec33f615923af
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2022-11-06 20:15:03 +0100

    Anotado un recordatorio

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+// para que se vean las recetas al cargar la pagina, debemos usar el operador $, que llame a pagRecetas
+
 function pagRecetas() {
     let content = document.getElementById('content');
     content.innerHTML = 'Recetas' 
