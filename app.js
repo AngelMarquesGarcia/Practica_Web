61f98013a734a4df4f5bb50b17e7f88acd4b10df
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2022-11-06 21:41:10 +0100

    created the template for the main objects

--- app.js
+++ app.js
@@ -3,7 +3,13 @@
 
 function pagRecetas() {
     let content = document.getElementById('content');
-    content.innerHTML = 'Recetas' 
+    
+    var nCompleteRows = listaRecetas.length() / 3
+    if (nCompleteRows <= 1 ) {
+        $(grid).css({grid-template-rows: [y0] 1fr [y1] 1fr [y2] 1fr [y3];})
+
+    }
+    content.innerHTML = <div id=""> </div>
 }
 
 
@@ -30,7 +36,47 @@ function pagBuscar() {
 }
 
 
+class objReceta {
+    constructor(n, d, i){
+        this.nombre=n
+        this.descripcion=d
+        this.ingredientes=i
+        }
 
+    getName(){
+        return this.nombre
+    }
+    getDescripcion(){
+        return this.descripcion
+    }
+    getIngredientes(){
+        return this.ingredientes
+    }
+}
+
+class objIngrediente {
+
+    constructor(n, d){
+        this.nombre=n
+        this.descripcion=d
+        }
+    getName(){
+        return this.nombre
+    }
+    getDescripcion(){
+        return this.descripcion
+    }
+
+}
+
+miel = objIngrediente.constructor("miel","dulce y pegajosa")
+a = objReceta.constructor("a","a",miel)
+b = objReceta.constructor("b","b",miel)
+c = objReceta.constructor("c","c",miel)
+d = objReceta.constructor("d","d",miel)
+
+
+listaRecetas=[a,b,c,d]
 
 
 
