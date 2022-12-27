9c8014693f956822e36ebde40495048f21d463df
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2022-12-27 19:34:35 +0100

    'Finalizado' la vista crearReceta y sus llamadas en el router

--- Parte 2/src/router.js
+++ Parte 2/src/router.js
@@ -23,8 +23,27 @@ router.get('/modificarReceta/:id', (req, res) => {
     let ingredientList = Array.from(mapaIngredientes.values())
     let recetaModificar = mapaRecetas.get(req.params.id)
 
-    res.render('crearReceta', {titulo: "Actualiza tu maravillosa receta!", ingredientes:ingredientList, receta:recetaModificar})
+    let pasos = mapaRecetas.get(req.params.id).pasos
+    let listaP = []
+    for (let c=0;c<pasos.length;c++){
+        listaP[c] = {indice:c,valor:pasos[c]}}
+    
+    let listaPasos = {length:pasos.length, lista:listaP}
+
+    let listaIngredientes = []
+    let c=0
+    for ([key,value] of mapaRecetas){
+        listaIngredientes[c] = {indiceOG:key,indiceActual:c,nombre:value}
+        c++
+    }
+    
+
+    res.render('crearReceta', {titulo: "Actualiza tu maravillosa receta!", ingredientes:ingredientList, receta:recetaModificar,objPasos:listaPasos,objListaIngrediente:listaIngredientes})
 });
+
+//objPasos = {length:n, lista:[{indice:0,valor:"Hello"}, {1:"World"}]}
+//objListaIngrediente = [{indiceOG:3,indiceActual:0,nombre:"juan"},{indiceOG:4,indiceActual:1,nombre:"a"}]
+
 
 router.get('/crearIngrediente', (req, res) =>{ 
     res.render('crearIngrediente', {titulo: "Crea un nuevo Ingrediente!", ingrediente:ingredientePlaceHolder})
